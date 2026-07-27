10c75ea1d9101b658b5de1aae9be882539ecdadf
michael-do-code <michaelnguyen86111@gmail.com>
2026-07-26 22:14:05 -0700

    Update footer

--- chasel/src/main/java/com/app/chasel/config/CorsConfig.java
+++ chasel/src/main/java/com/app/chasel/config/CorsConfig.java
@@ -14,6 +14,6 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
-                .maxAge(3600);
+                .maxAge(100);
     }
 }
