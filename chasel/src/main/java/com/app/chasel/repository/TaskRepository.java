e3ba54e335c13d3ea426eddfccf28bc9743b636e
Michael <michaelnguyen86111@gmail.com>
2026-09-24 13:01:09 -0700

    feat(frontend): refresh marketplace collection

--- chasel/src/main/java/com/app/chasel/repository/TaskRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.chasel.repository;
-
-import com.app.chasel.model.Task;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TaskRepository extends JpaRepository<Task, Long> {
-    
-}
--- chasel/src/main/java/com/app/chasel/service/TaskService.java
+++
@@ -1,5 +0,0 @@
-package com.app.chasel.service;
-
-public class TaskService {
-    
-}
